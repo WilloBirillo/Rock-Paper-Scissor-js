b25b0d87e411f1df4cace3b60445036e1b42475c
WilloBirillo <174868022+WilloBirillo@users.noreply.github.com>
2024-09-12 18:55:32 +0200

    Add: playRound function to define who wins

--- main.js
+++ main.js
@@ -11,10 +11,52 @@ function getComputerChoice() {
   }
   return computerChoice;
 }
-console.log(getComputerChoice());
+
 function getHumanChoice() {
   let humanChoice = prompt("Rock, Paper or Scissor");
   console.log(humanChoice);
   return humanChoice;
 }
-getHumanChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+humanChoice = humanChoice.toLowerCase();
+computerChoice = computerChoice.toLowerCase();
+
+function playRound(humanChoice, computerChoice) {
+  if (computerChoice === humanChoice) {
+    alert("The choices were the same, no one wins!");
+  } else if (computerChoice === "rock" && humanChoice === "paper") {
+    alert("Paper beats Rock, player wins!");
+    humanScore += 1;
+    return humanScore;
+  } else if (computerChoice === "rock" && humanChoice === "scissor") {
+    alert("Rock beats Scissor, cpu wins!");
+    computerScore += 1;
+    return computerScore;
+  } else if (computerChoice === "paper" && humanChoice === "rock") {
+    alert("Paper beats Rock, cpu wins!");
+    computerScore += 1;
+    return computerScore;
+  } else if (computerChoice === "paper" && humanChoice === "scissor") {
+    alert("Scissor beats Paper, player wins!");
+    humanScore += 1;
+    return humanScore;
+  } else if (computerChoice === "scissor" && humanChoice === "rock") {
+    alert("Rock beats Scissor, player wins!");
+    humanScore += 1;
+    return humanScore;
+  } else if (computerChoice === "scissor" && humanChoice === "paper") {
+    alert("Scissor beats Paper, cpu wins!");
+    computerScore += 1;
+    return computerScore;
+  }
+}
+
+playRound(humanChoice, computerChoice);
+console.log(computerScore);
+console.log(humanScore);
