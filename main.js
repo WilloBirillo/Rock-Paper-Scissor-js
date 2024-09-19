602242f0f5cabafac521fe3653295be8448f54a4
WilloBirillo <174868022+WilloBirillo@users.noreply.github.com>
2024-09-20 01:51:15 +0200

    Fix: remove button clears all the messages

--- main.js
+++ main.js
@@ -2,9 +2,25 @@ const rock_button = document.querySelector("#rock-button");
 const paper_button = document.querySelector("#paper-button");
 const scissor_button = document.querySelector("#scissor-button");
 
-console.log(rock_button.value);
-console.log(paper_button.value);
-console.log(scissor_button.value);
+//* Declaration of 2 variables to keep score of the game
+let humanScore = 0;
+let computerScore = 0;
+
+const information_container = document.querySelector("#information");
+const player_score_display = document.createElement("div");
+const cpu_score_display = document.createElement("div");
+const score_container = document.querySelector("#score");
+
+rock_button.addEventListener("click", playRound);
+paper_button.addEventListener("click", playRound);
+scissor_button.addEventListener("click", playRound);
+
+const endgame_button = document.createElement("button");
+endgame_button.classList.add("remove-button");
+endgame_button.textContent = "RESTART";
+
+score_container.appendChild(player_score_display);
+score_container.appendChild(cpu_score_display);
 
 function getComputerChoice() {
   let result = Math.floor(Math.random() * 3);
@@ -18,10 +34,6 @@ function getComputerChoice() {
   }
   return computerChoice;
 }
-
-//* Declaration of 2 variables to keep score of the game
-let humanScore = 0;
-let computerScore = 0;
 
 function playRound(event) {
   const humanChoice = event.target.value;
@@ -46,77 +58,70 @@ function playRound(event) {
   updateScores();
 
   if (humanScore == 5 || computerScore == 5) {
-
-    gameWinner(humanScore, computerScore);
+    gameWinner(endgame_button, humanScore, computerScore);
   }
 }
 
-rock_button.addEventListener("click", playRound);
-paper_button.addEventListener("click", playRound);
-scissor_button.addEventListener("click", playRound);
+function playerWin() {
+  const player_win = document.createElement("div");
+  player_win.textContent = "Good job you won";
+  information_container.appendChild(player_win);
+  updateMessage(player_win);
+}
 
-const information_container = document.querySelector("#information");
-const player_score_display = document.createElement("div");
-const cpu_score_display = document.createElement("div");
-const score_container = document.querySelector("#score");
-
-score_container.appendChild(player_score_display);
-score_container.appendChild(cpu_score_display);
-
-
-function updateRoundMessage(message) {
-  rock_button.addEventListener("click", () => message.remove());
-  paper_button.addEventListener("click", () => message.remove());
-  scissor_button.addEventListener("click", () => message.remove());
+function computerWin() {
+  const computer_win = document.createElement("div");
+  computer_win.textContent = "Too bad you lost";
+  information_container.appendChild(computer_win);
+  updateMessage(computer_win);
 }
 
 function tieRoundMessage() {
   const tie_message = document.createElement("div");
   tie_message.textContent = "Close enough, no one wins the round! 👺"
   information_container.appendChild(tie_message);
-  updateRoundMessage(tie_message)
+  updateMessage(tie_message)
 }
 
 function playerRoundMessage() {
   const player_message = document.createElement("div");
   player_message.textContent = "Good job, player wins the round! 👍";
   information_container.appendChild(player_message);
-  updateRoundMessage(player_message);
+  updateMessage(player_message);
 }
 
 function cpuRoundMessage() {
   const cpu_message = document.createElement("div");
   cpu_message.textContent = "Too bad, cpu wins the round! 💩";
   information_container.appendChild(cpu_message);
-  updateRoundMessage(cpu_message);
+  updateMessage(cpu_message);
 }
 
-function removeRoundMessage() {
-  player_message.remove();
-  cpu_message.remove();
+//* Used to clear all the messages
+function updateMessage(message) {
+  rock_button.addEventListener("click", () => message.remove());
+  paper_button.addEventListener("click", () => message.remove());
+  scissor_button.addEventListener("click", () => message.remove());
+  endgame_button.addEventListener("click", () => message.remove());
 }
 
-function gameWinner(humanScore, computerScore) {
-  const endgame_message = document.createElement("div");
-  const endgame_button = document.createElement("button");
-  endgame_button.classList.add("remove-button");
+function updateScores() {
+  player_score_display.textContent = `Player score: ${humanScore}`;
+  cpu_score_display.textContent = `Cpu score: ${computerScore}`;
+}
 
+function gameWinner(endgame_button, humanScore, computerScore) {
   if (humanScore == 5) {
-    endgame_message.textContent = "Good job you won";
-    information_container.appendChild(endgame_message);
+    playerWin();
   } else if (computerScore == 5) {
-    endgame_message.textContent = "Too bad you lost";
-    information_container.appendChild(endgame_message);
+    computerWin();
   }
   disableButtons();
-  endgame_button.textContent = "RESTART";
-
+  
   information_container.appendChild(endgame_button);
   endgame_button.addEventListener("click", () => {
     resetGame();
-
     endgame_button.remove();
-    endgame_message.remove();
   });
 }
 
@@ -133,11 +138,6 @@ function resetGame() {
   rock_button.disabled = false;
   paper_button.disabled = false;
   scissor_button.disabled = false;
-}
-
-function updateScores() {
-  player_score_display.textContent = `Player score: ${humanScore}`;
-  cpu_score_display.textContent = `Cpu score: ${computerScore}`;
 }
 
 updateScores();
